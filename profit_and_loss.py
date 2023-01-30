7c07db24d45ebfbabc5e4539086a248b52b7655d
garekoool <garekoool@gmail.com>
2023-01-30 22:01:45 +0800

    commit

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -1,6 +1,8 @@
+#import csv module
 import csv
 
 def profit_loss():
+    #Lists and Variables
     profitandloss = []
     profit_results = 0
     profit_details = 0
@@ -17,18 +19,27 @@ def profit_loss():
         next(reader)
 
 
-    # Create nested loop to access each value in the list
-    # and append the value to the dict.
+        #Create nested loop to access each value in the list and append the value to the profitandloss list.
         for line in reader:
                 for value in line:
                     profitandloss.append(value)
+    #The 'i' in the loop represents the position of the value in the list
+    #The loop starts from 4th number in the list and only takes the net profit of each day which are in intervals of 5 in the profitandloss list               
     for i in range(4,len(profitandloss)-4,5):
+        #if the value of the current day is higher than the value of the previous day
         if float(profitandloss[i+5]) - float(profitandloss[i])>0:
+            #it will add 1 into the profit_surplus variable
             profit_surplus = profit_surplus + 1   
+        #else if the value of the current day is lower or equal to the value of the previous day
         elif int(profitandloss[i+5]) - int(profitandloss[i])<=0:
+            #the day when the value of the current day is lower or equal to the value of the previous day
+            #is appended into the profit_deficit_day list
             profit_deficit_day.append(profitandloss[i+1])
+            #the amount when the value of the current day is lower or equal to the value of the previous day
+            #is appended into the profit_deficit_amt list
             profit_deficit_amt.append(abs(int(profitandloss[i+5])-int(profitandloss[i])))
-   
+    #The if else will summarise the results and determine whether if all of the days were more or less than the previous 
+    #if its neither, it has an mixed number of losses and surpluses then it will be categorised as profit deficit
     if profit_surplus == len(profitandloss)/2:
         profit_results = "HIGHER"
         profit_details = "NET PROFIT SURPLUS"
@@ -39,7 +50,8 @@ def profit_loss():
         profit_details = "PROFIT DEFICIT" 
     
 
-
+    #Iterates items in the list and prints a message showing details of  
+    #whether it is a net profit surplus, net profit loss or profit deficit with the amounts and day on the text file
     with open("summary_report.txt", "a") as f:
         
         if profit_results == 0:
